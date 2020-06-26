045cd02b2140b24fb4ece31f8aa79c99e92d414c
alex.paclin <alex.paclin@gmail.com>
2020-06-26 14:57:57 +0500

    Redux Init

--- src/App.js
+++ src/App.js
@@ -8,28 +8,33 @@ import {
 	Login as LoginPage,
 	Register as RegisterPage,
 } from 'pages';
-import { Navbar, NavbarClone } from 'components';
+import { Navbar, NavbarClone, Sidebar } from 'components';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
+import { Provider } from 'react-redux';
 import React from 'react';
-import { Sidebar } from 'components';
+import initStore from 'store';
+
+const store = initStore();
 
 function App() {
 	return (
 		<div>
-			<Router>
-				<Navbar id="navbar-clone" />
-				<NavbarClone />
-				<Sidebar />
-				<Switch>
-					<Route path="/register" component={RegisterPage} />
-					<Route path="/login" component={LoginPage} />
-					<Route path="/services" component={ServicesPage} />
-					<Route path="/profile" component={ProfilePage} />
-					<Route path="/faq" component={FaqPage} />
-					<Route exact path="/" component={HomePage} />
-				</Switch>
-			</Router>
+			<Provider store={store}>
+				<Router>
+					<Navbar id="navbar-clone" />
+					<NavbarClone />
+					<Sidebar />
+					<Switch>
+						<Route path="/register" component={RegisterPage} />
+						<Route path="/login" component={LoginPage} />
+						<Route path="/services" component={ServicesPage} />
+						<Route path="/profile" component={ProfilePage} />
+						<Route path="/faq" component={FaqPage} />
+						<Route exact path="/" component={HomePage} />
+					</Switch>
+				</Router>
+			</Provider>
 		</div>
 	);
 }
