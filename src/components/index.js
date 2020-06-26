3ed1c958875bf9436cefda640644732b9f7c96fb
alex.paclin <alex.paclin@gmail.com>
2020-06-26 10:33:14 +0500

    Service Item Component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
+import Hero from './Hero/Hero';
 import Navbar from './Navbar/Navbar';
 import NavbarClone from './NavbarClone/NavbarClone';
+import ServiceItem from './ServiceItem/ServiceItem';
 import Sidebar from './Sidebar/Sidebar';
 
-export { Navbar, NavbarClone, Sidebar };
+export { Navbar, NavbarClone, Sidebar, Hero, ServiceItem };
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,6 @@
 /* eslint jsx-a11y/anchor-is-valid: 0 */
 
-import { Navbar, NavbarClone } from 'components';
+import { Hero, Navbar, NavbarClone, ServiceItem } from 'components';
 import React, { useEffect, useState } from 'react';
 
 import { getServices } from 'store';
@@ -15,31 +15,10 @@ const Home = () => {
 		setState({ services });
 	}, []);
 
-	const renderServices = (services) => {
-		return services.map((service) => (
-			<div key={service.id} className="column is-one-third">
-				<div
-					className="feature-card is-bordered has-text-centered revealOnScroll delay-1"
-					data-animation="fadeInLeft"
-				>
-					<div className="card-title">
-						<h4>{service.title}</h4>
-					</div>
-					<div className="card-icon">
-						<img src={service.image} alt="" />
-					</div>
-					<div className="card-text">
-						<p>{service.description}</p>
-					</div>
-					<div className="card-action">
-						<a href="#" className="button btn-align-md accent-btn raised">
-							Learn More
-						</a>
-					</div>
-				</div>
-			</div>
+	const renderServices = (services) =>
+		services.map((service) => (
+			<ServiceItem key={service.id} service={service} />
 		));
-	};
 
 	const { services } = state;
 
@@ -47,36 +26,7 @@ const Home = () => {
 		<div>
 			<Navbar />
 			<NavbarClone />
-			<section className="hero is-default is-bold">
-				<div className="hero-body">
-					<div className="container">
-						<div className="columns is-vcentered">
-							<div className="column is-5 is-offset-1 landing-caption">
-								<h1 className="title is-1 is-bold is-spaced">
-									Learn, Collaborate.
-								</h1>
-								<h2 className="subtitle is-5 is-muted">
-									Lorem ipsum sit dolor amet is a dummy text used by typography
-									industry{' '}
-								</h2>
-								<p>
-									<a className="button cta rounded primary-btn raised">
-										Get Started
-									</a>
-								</p>
-							</div>
-							<div className="column is-5 is-offset-1">
-								<figure className="image is-4by3">
-									<img
-										src={process.env.PUBLIC_URL + '/worker.svg'}
-										alt="Description"
-									/>
-								</figure>
-							</div>
-						</div>
-					</div>
-				</div>
-			</section>
+			<Hero />
 
 			<section className="section section-feature-grey is-medium">
 				<div className="container">
