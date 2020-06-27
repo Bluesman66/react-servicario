dea98f313352d24693e2418c6a8fe48378ffc780
alex.paclin <alex.paclin@gmail.com>
2020-06-27 21:44:36 +0500

    Improvements + Caching

--- src/actions/index.js
+++ src/actions/index.js
@@ -14,8 +14,12 @@ export const fetchServices = () => (dispatch) =>
 		})
 	);
 
-export const fetchServiceById = (serviceId) => (dispatch) => {
-	dispatch({ type: FETCH_SERVICE_SUCCESS, service: {} });
+export const fetchServiceById = (serviceId) => (dispatch, getState) => {
+	const lastService = getState().selectedService.item;
+	if (lastService.id && lastService.id === serviceId) {
+		return Promise.resolve();
+	}
+
 	dispatch({ type: REQUEST_SERVICE });
 	return api.fetchServiceById(serviceId).then((service) =>
 		dispatch({
