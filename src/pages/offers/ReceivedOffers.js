7dc8cae03cee4df4278f3d71dba286a169e522d1
alex.paclin <alex.paclin@gmail.com>
2020-07-07 11:08:58 +0500

    Accept & Decline Offer Buttons

--- src/pages/offers/ReceivedOffers.js
+++ src/pages/offers/ReceivedOffers.js
@@ -11,6 +11,20 @@ const ReceivedOffers = (props) => {
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
+	const acceptOffer = (offer) => {
+		console.log(`Accepting ${offer}`);
+	};
+
+	const declineOffer = (offer) => {
+		console.log(`Declining ${offer}`);
+	};
+
+	const statusClass = (status) => {
+		if (status === 'pending') return 'is-warning';
+		if (status === 'accepted') return 'is-success';
+		if (status === 'declined') return 'is-danger';
+	};
+
 	const { offers } = props;
 	return (
 		<div className="container">
@@ -24,7 +38,9 @@ const ReceivedOffers = (props) => {
 								className="offer-card"
 								service={offer.service}
 							>
-								<div className="tag is-large">{offer.status}</div>
+								<div className={`tag is-large ${statusClass(offer.status)}`}>
+									{offer.status}
+								</div>
 								<hr />
 								<div className="service-offer">
 									<div>
@@ -41,6 +57,23 @@ const ReceivedOffers = (props) => {
 										<span className="label">Time:</span> {offer.time} hours
 									</div>
 								</div>
+								{offer.status === 'pending' && (
+									<div>
+										<hr />
+										<button
+											onClick={() => acceptOffer(offer)}
+											className="button is-success s-m-r"
+										>
+											Accept
+										</button>
+										<button
+											onClick={() => declineOffer(offer)}
+											className="button is-danger"
+										>
+											Decline
+										</button>
+									</div>
+								)}
 							</ServiceItem>
 						</div>
 					))}
