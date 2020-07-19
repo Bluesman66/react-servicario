2bf78bc3de82ed080e6a69df79cc6887b82ed9b5
alex.paclin <alex.paclin@gmail.com>
2020-07-19 09:52:45 +0500

    more rules

--- src/pages/collaborations/CollaborationDetail.js
+++ src/pages/collaborations/CollaborationDetail.js
@@ -184,14 +184,14 @@ const CollaborationDetail = (props) => {
 										setState({ ...state, inputValue: e.target.value })
 									}
 									onKeyPress={onKeyboardPress}
-									disabled={status === 'finished' || status === 'notStarted'}
+									//disabled={status === 'finished' || status === 'notStarted'}
 									value={inputValue}
 									className="viewInput"
 									placeholder="Type your message..."
 								/>
 								<button
 									onClick={() => onSendMessage(inputValue)}
-									disabled={status === 'finished' || status === 'notStarted'}
+									//disabled={status === 'finished' || status === 'notStarted'}
 									className="button is-primary is-large"
 								>
 									Send
